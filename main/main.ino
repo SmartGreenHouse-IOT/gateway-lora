640608f3f3451261dcb6bd7389382301fc1b9e02
Poodle Moth <110733000+WhitePoodleMoth@users.noreply.github.com>
2024-08-14 13:48:25 -0300

    feat: add lora do main

--- main/main.ino
+++ main/main.ino
@@ -2,6 +2,7 @@
 #include "logger.h"
 #include "connection.h"
 #include "wireless.h"
+#include "lora.h"
 
 // Configuração do Display OLED
 SSD1306Wire factory_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED);
@@ -16,15 +17,17 @@ SSD1306Wire factory_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, R
 
 void setup() {
     setupLogger(); // Inicializa o logger e o display
+    setupLoRa();
 
     // Conectar ao Wi-Fi
     connectToWiFi(WIRELESS_SSID, WIRELESS_PSWD);
 }
 
 void loop() {
+    String msg = receivePacket()
     if (isConnectedToWiFi()) {
-        SendSensorData(GATEWAY_SERIAL_DEVELOPMENT, SLAVE_SERIAL_DEVELOPMENT, 123, 456, 7890, 23.45, 6789, 234, 56.78);
+        // SendSensorData(GATEWAY_SERIAL_DEVELOPMENT, SLAVE_SERIAL_DEVELOPMENT, 123, 456, 7890, 23.45, 6789, 234, 56.78);
         // RecordData(GATEWAY_SERIAL_DEVELOPMENT, SLAVE_SERIAL_DEVELOPMENT, "123456789023.45678923456.78");
     }
-    delay(5000);
+    delay(100);
 }
