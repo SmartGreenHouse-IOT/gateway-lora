157eb8aff745c29a61607228649bc28ccc694ab0
rafaelnakayama25 <111544734+tlife777@users.noreply.github.com>
2024-08-22 11:20:44 -0300

    feat: created a function for contentor datas
    Co-Authored-By: White Poodle Moth <110733000+WhitePoodleMoth@users.noreply.github.com>

--- main/connection.h
+++ main/connection.h
@@ -35,6 +35,36 @@ void RecordData(String rx, String tx, String data) {
   logInfo("Conexão encerrada");
 }
 
+int ContentorRecordData(float latitude, float longitude, float bateria, float volume) {
+  HTTPClient http;
+
+  logInfo("Conectando com a API...");
+  http.begin(CONTENTOR_ENDPOINT);
+
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  String payload = "latitude=" + String(latitude, 6) +
+                 "&longitude=" + String(longitude, 6) +
+                 "&bateria=" + String(bateria, 2) +
+                 "&volume=" + String(volume, 2);
+
+  int httpResponseCode = http.POST(payload);
+
+  if (httpResponseCode > 0) {
+    String response = http.getString();
+    logSuccess("Dados enviados com sucesso");
+    logSuccess(response);
+  } else {
+    logError("Erro na requisição POST");
+    logError(http.errorToString(httpResponseCode));
+  }
+
+  http.end();
+  logInfo("Conexão encerrada");
+
+  return httpResponseCode;
+}
+
 void SendSensorData(const char* rx, const char* tx, int temperature, int humidity, int total_volume_ml, float flow_rate_lps, int flow_volume_ml, int soil_moisture, float light_intensity) {
   // Montar a string data com os parâmetros fornecidos
   String data = String(temperature) +
